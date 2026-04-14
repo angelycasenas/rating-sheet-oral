483c320b820d9ab3674b929ceb8b1667d73a9a50
angelycasenas <angelycasenas042@gmail.com>
2026-04-14 13:44:28 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+// config.js
+
+// 1. PALIHOG KOG BUTANG SA URL WEB APP SCRIPT DRI DAPITA
+const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzd88uKL0lT_GDATWgrC87wtB2lQqJGPmnFMKwEXtbVDWtqqJpTYJFPexrqOvgUY4hhdQ/exec";
+
+// 2. DRI BUTANG ANG GADTONG GROUP MEMBERS 
+const groupMembers = [
+  " Angely M. Caseñas",
+  "Ray-lyn Togayong",
+  "Zaira M. Gonzales",
+  "Emmalyn Inutan",
+  "Jessamae Cabaong",
+  "Lorenz Palero",
+   "Jefrey Mahinay"
+];
+
+// 3. DRI IBUTANG ANG KADTONG MGA PROJECT TITLES 
+const projectTitles = [
+  "AniCheck: An Android-Based Real-Time Freshness Detection of Meat and Chicken Using Colorimetric Analysis",
+  "Development of a Web-Based Sports Event Management System with Live Streaming Integration",
+  "Fever Detection with Embedded Automatic Medication System"
+];
